a3cc97c68f1808fa79059b33189506dc0adec740
firoz3130 <firosk7@gmail.com>
2024-03-24 23:50:53 +0530

    Fixed type issue in blog/id fe page

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,12 +4,11 @@ import { Spinner } from "../components/Spinner";
 import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 
-// atomFamilies/selectorFamilies
 export const Blog = () => {
     const { id } = useParams();
     const { loading, blog } = useBlog({
         id: id || ""
-    });
+    }) as { loading: boolean; blog: Blog | undefined };
 
     if (loading || !blog) {
         return <div>
