debcaf370e62523aab443001c2cf6203795c396c
firoz3130 <firosk7@gmail.com>
2024-04-10 20:59:32 +0530

    this is to test the commi tcommand i

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ function App() {
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/publish" element={<Publish />} />
+
         </Routes>
       </BrowserRouter>
     </>
